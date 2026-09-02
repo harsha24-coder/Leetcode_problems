5a155a41fe239829a5ccae3f3d0d2d5a3c797feb
harsha24-coder <harsha.doddi24@gmail.com>
2026-09-02 10:07:33 +0530

    Time: 20 ms (83.28%), Space: 56.2 MB (94.39%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        
+        Arrays.sort(people);
+        int left =0,count=0;
+        int right = people.length-1;
+        while(left<= right){
+            int sum=people[left] + people[right];
+            if(sum <= limit){
+              left++;
+            }
+            right--;
+            count++;
+        }
+        return count;
+    }
+}
